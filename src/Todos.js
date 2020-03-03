88d8ed27fa865e95ea6030232510baaa5f5016f0
Anıl Şenay <anilsenay3@gmail.com>
2020-03-03 18:39:05 +0300

    some css changes

--- src/Todos.js
+++ src/Todos.js
@@ -4,16 +4,16 @@ const Todos = ({todos, deleteTodo}) => {
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
-                <div className = "collection-item" key = {todo.id}>
-                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+                <div className = "collection-item-margin" key = {todo.id}>
+                    <span className = "test" onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
                 </div>
             )
         })
     ) : (
-        <p className = "center">You have no todos left </p>
+        <p className = "center no-left">You have no todos left </p>
     )
     return (
-        <div className = "todos collection">
+        <div className = "todos collection no-border">
             {todoList}
         </div>
     )
